d6c6715979949d76c2768521fabc718de0d28271
sharmasourabh@users.noreply.github.com <sousharm>
2021-02-09 22:32:16 +0530

    bump dependencies laravel-mix 6, tailwind 2, postcss 8 and so on

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
+const path = require("path");
 
 // Paths
 const paths = {
@@ -9,7 +10,6 @@ const paths = {
   },
   javascript: {
     source: "./src/resources/js/main.js",
-    singles: "./src/resources/js/singles/*",
     dest: "dist/src/js/",
   },
 };
@@ -29,17 +29,9 @@ mix
   // Concatenate & Compile Javascript
   .js(paths.javascript.source, paths.javascript.dest)
 
-  // Compile singles
-  // .js(paths.javascript.singles, paths.javascript.dest)
-
   // Compile SCSS & TailwindCSS
   .sass(paths.sass.source, paths.sass.dest)
   .options({
     processCssUrls: false,
     postCss: [tailwindcss("./tailwind.config.js")],
   });
-
-mix
-  // Minifies CSS & JS files
-  .minify(paths.sass.dest + "main.css")
-  .minify(paths.javascript.dest + "main.js");
